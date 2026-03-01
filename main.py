476ef015d4b141f68d863f8f22bd1ac1eac9b74b
MaleBrochero <marialeal2651@gmail.com>
2026-03-01 17:33:26 -0500

    nueva actualizacion

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
-# PUNTOS DE VENTAS
+#PUNTOS DE VENTAS
 print("Bienvenido a RiwiTechStore, es un placer para nosotro servirle :)...")
-nombre_cliente = input("Por favor ingrese su nombre y apellido ejem: Aurora Quintero")
-
-
-afirmacion = int(input("¿Desear llevar este producto ? Ingrese solo estos numeros  1.SI  2.NO"))
+nombre_cliente = input("Por favor ingrese su nombre y apellido ejem: Aurora Quintero ")
+afirmacion = int(input("¿Desear llevar este producto ? Ingrese solo estos numeros  1.SI  2.NO "))
 
 if afirmacion == 1:
-
-        while True:
-            nombre_producto = input("Por favor ingrese el nombre del producto ")
-            valor_producto = int("Ingrese el valor del producto ")
-            print("Nombre del Producto ", nombre_producto," Precio ", valor_producto)
-        else:
+    
+        while afirmacion == 1:
+            nombre_producto = input("Por favor ingrese el nombre del producto  ")
+            valor_producto = int(input("Ingrese el valor del producto "))
             total_valor_producto = valor_producto + valor_producto
-            Cliente_VIP = int(input("¿Es cliente VIP? Ingrese opcion valida 1.SI 2.NO "))
-            print("Gracias por su compra", Nombre_Cliente,
-                "El total de su compra es ", total_valor_producto)
-
-elif afirmacion == 2:
+            print("Nombre del Producto ", nombre_producto," Precio ", valor_producto)
+            afirmacion = int(input("¿Desear llevar este producto ? Ingrese solo estos numeros  1.SI  2.NO "))
+            
+        else:
+            cliente_vip = int(input("¿Es cliente VIP? Ingrese opcion valida 1.SI 2.NO "))
+            
+            
+            if cliente_vip == 1:
+                descuento_vip = total_valor_producto*0.80
+                print("Gracias por su compra", nombre_cliente,
+                "El total de su compra es ", descuento_vip)
+                
+            else:
+                print("Gracias por su compra", nombre_cliente,"El total de su compra es ", total_valor_producto)
+if afirmacion == 2:
     print("Gracias por su venir", nombre_cliente)
+
 else:
     print("No ha ingresado un numero valido")
