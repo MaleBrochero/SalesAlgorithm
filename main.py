11e2699ff35eee05412900a5761c6be2091341bb
MaleBrochero <marialeal2651@gmail.com>
2026-03-04 21:55:57 -0500

    Codigo completo

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 #PUNTOS DE VENTAS
+
+
 print("Bienvenido a RiwiTechStore, es un placer para nosotro servirle :)...")
 nombre_cliente = input("Por favor ingrese su nombre y apellido ejem: Aurora Quintero ")
-afirmacion = int(input("¿Desear llevar este producto ? Ingrese solo estos numeros  1.SI  2.NO "))
+nombre_producto = input("Por favor ingrese el nombre del producto  ")
 
-if afirmacion == 1:
+try:
+
+    valor_producto = int(input("Ingrese el valor del producto en numeros "))
+    cantidad_producto = int(input("Ingrese  cantidad del producto en unidades : Ejempo  1, 2, 3, ... "))
+    cliente_vip = int(input("¿Es cliente VIP? Ingrese opcion valida 1.SI 2.NO "))
+    valor_total = valor_producto*cantidad_producto
     
-        while afirmacion == 1:
-            nombre_producto = input("Por favor ingrese el nombre del producto  ")
-            valor_producto = int(input("Ingrese el valor del producto "))
-            total_valor_producto = valor_producto + valor_producto
-            print("Nombre del Producto ", nombre_producto," Precio ", valor_producto)
-            afirmacion = int(input("¿Desear llevar este producto ? Ingrese solo estos numeros  1.SI  2.NO "))
-            
-        else:
-            cliente_vip = int(input("¿Es cliente VIP? Ingrese opcion valida 1.SI 2.NO "))
-            
-            if cliente_vip == 1:
-                descuento_vip = total_valor_producto*0.80
-                print("Gracias por su compra", nombre_cliente,
-                "El total de su compra es ", descuento_vip)
-                
-            elif cliente_vip == 2 :
-                print("Gracias por su compra", nombre_cliente,"El total de su compra es ", total_valor_producto)
-if afirmacion == 2:
-    print("Gracias por su venir", nombre_cliente)
-
-else:
-    print("No ha ingresado un numero valido")
+    if cliente_vip == 1:
+        valor_final_pagar = valor_total *0.9
+        valor_descuento = valor_total *0.1
+        print("El cliente ",nombre_cliente, "su descuento es ", valor_descuento , "precio total a pagar ", valor_final_pagar)
+        
+    elif cliente_vip == 2:
+        print ("El cliente ",nombre_cliente,  "precio total a pagar ",valor_total)
+    else :
+        print("No ingreso un valor valido")
+        
+        
+except :
+    print("Caracter invalido")
