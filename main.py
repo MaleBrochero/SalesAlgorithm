db1fe0d7fee89731c9bf19d4e9ea14c733ff6bf2
Maria Jose Leal <marialeal2651@gmail.com>
2026-03-06 08:44:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 #PUNTOS DE VENTAS
 
-
+#To init the program, you must ejecute py main.py...
 print("Bienvenido a RiwiTechStore, es un placer para nosotro servirle :)...")
 nombre_cliente = input("Por favor ingrese su nombre y apellido ejem: Aurora Quintero ")
 nombre_producto = input("Por favor ingrese el nombre del producto  ")
 
 try:
-
-    valor_producto = int(input("Ingrese el valor del producto en numeros "))
     cantidad_producto = int(input("Ingrese  cantidad del producto en unidades : Ejempo  1, 2, 3, ... "))
+    valor_producto = int(input("Ingrese el valor del producto en numeros "))
     cliente_vip = int(input("¿Es cliente VIP? Ingrese opcion valida 1.SI 2.NO "))
     valor_total = valor_producto*cantidad_producto
     
     if cliente_vip == 1:
         valor_final_pagar = valor_total *0.9
         valor_descuento = valor_total *0.1
-        print("El cliente ",nombre_cliente, "su descuento es ", valor_descuento , "precio total a pagar ", valor_final_pagar)
+        print (f"""El cliente {nombre_cliente}
+            Compró {nombre_producto } 
+            Cantidad en unidades {cantidad_producto} 
+            precio total  {valor_total} Descuento 10% {valor_descuento}
+            Nuevo total a pagar  {valor_final_pagar}
+            Gracias por su compra :) """)
         
     elif cliente_vip == 2:
-        print ("El cliente ",nombre_cliente,  "precio total a pagar ",valor_total)
+        print (f"""El cliente {nombre_cliente} 
+                Compró {nombre_producto } 
+                Cantidad en unidades {cantidad_producto} 
+                precio total a pagar {valor_total}
+                Gracias por su compra :)""")
     else :
         print("No ingreso un valor valido")
         
